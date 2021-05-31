d0c72e07d7be93eedce78afc35bc407e94ead90f
Noah  Klem <klem.noah@gmail.com>
2021-05-31 19:42:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a , b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  return array.reverse!
+end
+
+def kesha_maker(array)
+  kesha_results = []
+  array.each {|name| name[2] = "$"; kesha_results<< name}
+  return kesha_results
+end
+
+def find_a(array)
